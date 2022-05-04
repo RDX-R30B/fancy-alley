ecab51e49a94606682143b2f135ee33269d69bd0
RDX-R30B <ritwick.bisht88@gmail.com>
2022-05-04 15:57:44 +0530

    code added

---
+++ backend/qrgenerator/qrcode.js
@@ -0,0 +1,39 @@
+const spawn = require('child_process').spawn
+// const qrcode =  (url,callback) => {
+// //    var workerProcess = spawn('pipenv', ['run', 'python',`${__dirname}/qrcode.py`,url]);
+//     var workerProcess = spawn('ping',['www.google.com']);
+//     let result='';
+//     workerProcess.stdout.on('data', function (data) {
+//     //   console.log('stdout: ' + data);
+//         result += data;
+//     });
+
+//     workerProcess.on('close', function (code ) {
+//        return callback(result);
+//     });
+// }
+
+// qrcode("ritwicksdfghgjhklk;dsfghjkljhgfdsfghjkjhgfdsfghqweqwetyutuicvxcvasasagj", (result) => console.log(result))
+
+
+const qrcode = (url) => {
+    return new Promise((resolve,reject) => {
+        var workerProcess = spawn('pipenv', ['run', 'python',`${__dirname}/qrcode.py`,url]);
+        var result = '';
+        workerProcess.stdout.on('data',(data) => {
+            result+=data;
+        });
+        workerProcess.on('close',(code) => {
+                resolve(result);
+            }
+        )
+        workerProcess.on('error',(err) => {
+            reject(err)
+        })
+    })
+}
+
+// (async() => {
+//     console.log(await qrcode("ritwick"));
+// })()
+module.exports = qrcode
