6974eb29c980e789dfc14feca0bd63e55edf1a51
RDX-R30B <ritwick.bisht88@gmail.com>
2022-05-09 13:19:55 +0530

    ho ja bhai

---
+++ frontend/build/precache-manifest.dc18d83ff71c001c2a851c7cb2c396cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33fc5752c0dd174203df",
+    "url": "/static/css/main.a7cb0fc6.chunk.css"
+  },
+  {
+    "revision": "33fc5752c0dd174203df",
+    "url": "/static/js/main.33fc5752.chunk.js"
+  },
+  {
+    "revision": "833fff91d90f286c54ea",
+    "url": "/static/css/1.2ceb9997.chunk.css"
+  },
+  {
+    "revision": "833fff91d90f286c54ea",
+    "url": "/static/js/1.833fff91.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f49f17d0d989e01223f698229218694",
+    "url": "/index.html"
+  }
+];
